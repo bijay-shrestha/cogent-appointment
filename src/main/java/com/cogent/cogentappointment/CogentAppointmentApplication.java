6b4760cfe5302365c3dacbdc21aaa4fd4a7c674c
Bijay Shrestha <bijay.shrestha@f1soft.com>
2020-01-09 10:35:18 +0545

    First Commit.

---
+++ src/main/java/com/cogent/cogentappointment/CogentAppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.cogent.cogentappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogentAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogentAppointmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cogent/cogentappointment/CogentAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cogent.cogentappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogentAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

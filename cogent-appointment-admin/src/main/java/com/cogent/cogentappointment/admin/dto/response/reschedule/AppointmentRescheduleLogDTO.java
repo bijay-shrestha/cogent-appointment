f52973ea6fb1c3da455f3355c69617771c6c5a72
rupakchaulagain <rupakchaulagain@gmail.com>
2020-02-13 16:22:37 +0545

    Appointemnt Reschedule Log Completed...
    Took 2 minutes

---
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/dto/response/reschedule/AppointmentRescheduleLogDTO.java
@@ -0,0 +1,49 @@
+package com.cogent.cogentappointment.admin.dto.response.reschedule;
+
+import com.cogent.cogentappointment.persistence.enums.Gender;
+import lombok.*;
+
+import java.util.Date;
+
+/**
+ * @author Rupak
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppointmentRescheduleLogDTO {
+
+    private String esewaId;
+
+    private String hospitalName;
+
+    private Date previousAppointmentDate;
+
+    private Date rescheduleAppointmentDate;
+
+    private String appointmentNumber;
+
+    private String registrationNumber;
+
+    private String patientName;
+
+    private String patientAddress;
+
+    private Gender patientGender;
+
+    private String patientAge;
+
+    private String mobileNumber;
+
+    private String doctorName;
+
+    private String specializationName;
+
+    private String transactionNumber;
+
+    private Double appointmentAmount;
+
+    private String remarks;
+}

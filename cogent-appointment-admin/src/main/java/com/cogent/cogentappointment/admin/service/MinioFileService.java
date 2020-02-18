2c81a7160cbdc97e95b03017c0f1edcefe0b6cfa
rupakchaulagain <rupakchaulagain@gmail.com>
2020-02-18 15:07:09 +0545

    FILE URL Issue Solved...
    Took 7 minutes

---
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/service/MinioFileService.java
@@ -0,0 +1,28 @@
+package com.cogent.cogentappointment.admin.service;
+
+import com.cogent.cogentappointment.admin.dto.response.files.FileUploadResponseDTO;
+import com.jlefebure.spring.boot.minio.MinioException;
+import io.minio.messages.Item;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author Rupak
+ */
+public interface MinioFileService {
+
+    void addAttachment(MultipartFile file);
+
+    List<FileUploadResponseDTO> addAttachmentIntoSubDirectory(String subDirectory, MultipartFile[] file);
+
+    List<Item> getAllList() throws MinioException;
+
+    InputStream getObject(String object) throws MinioException;
+
+    InputStream getObjectWithSubDirectory(String subDirectory, String object) throws MinioException;
+
+    void deleteFiles(String object) throws MinioException;
+
+}

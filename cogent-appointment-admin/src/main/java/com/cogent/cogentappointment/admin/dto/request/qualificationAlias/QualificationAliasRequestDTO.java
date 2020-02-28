17b03bf720cefb9507d4bd3123a4d512c04648b6
rupakchaulagain <rupakchaulagain@gmail.com>
2020-02-28 16:05:50 +0545

    Qualification Alias CRUD Completed...

---
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/dto/request/qualificationAlias/QualificationAliasRequestDTO.java
@@ -0,0 +1,29 @@
+package com.cogent.cogentappointment.admin.dto.request.qualificationAlias;
+
+import com.cogent.cogentappointment.admin.constraintvalidator.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Rupak
+ */
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class QualificationAliasRequestDTO {
+
+    @NotNull
+    @NotEmpty
+    private String name;
+
+    @NotNull
+    @Status
+    private Character status;
+
+}
---
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/utils/QualificationAliasUtils.java
@@ -0,0 +1,36 @@
+package com.cogent.cogentappointment.admin.utils;
+
+import com.cogent.cogentappointment.admin.dto.commons.DeleteRequestDTO;
+import com.cogent.cogentappointment.admin.dto.request.qualificationAlias.QualificationAliasRequestDTO;
+import com.cogent.cogentappointment.admin.dto.request.qualificationAlias.QualificationAliasUpdateRequestDTO;
+import com.cogent.cogentappointment.persistence.model.QualificationAlias;
+
+import static com.cogent.cogentappointment.admin.utils.commons.StringUtil.toUpperCase;
+
+/**
+ * @author Rupak
+ */
+public class QualificationAliasUtils {
+
+    public static QualificationAlias parseToQualificationAlias(QualificationAliasRequestDTO requestDTO) {
+        QualificationAlias qualificationAlias = new QualificationAlias();
+        qualificationAlias.setName(toUpperCase(requestDTO.getName()));
+        qualificationAlias.setStatus(requestDTO.getStatus());
+        return qualificationAlias;
+    }
+
+    public static QualificationAlias parseToUpdatedQualificationAlias(QualificationAlias qualificationAlias, QualificationAliasUpdateRequestDTO requestDTO) {
+
+        qualificationAlias.setName(toUpperCase(requestDTO.getName()));
+        qualificationAlias.setStatus(requestDTO.getStatus());
+        qualificationAlias.setRemarks(requestDTO.getRemarks());
+        return qualificationAlias;
+    }
+
+    public static void parseToDeletedQualification(QualificationAlias qualificationAlias, DeleteRequestDTO deleteRequestDTO) {
+
+        qualificationAlias.setStatus(deleteRequestDTO.getStatus());
+        qualificationAlias.setRemarks(deleteRequestDTO.getRemarks());
+
+    }
+}

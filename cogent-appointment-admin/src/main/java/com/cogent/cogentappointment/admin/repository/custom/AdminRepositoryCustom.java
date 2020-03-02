aeb5e2fb9a20c770e6407544a568013679321fe7
f1soft <you@example.com>
2020-03-02 16:19:04 +0545

    fix hmac(IsCogentADmin)

--- cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/repository/custom/AdminRepositoryCustom.java
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/repository/custom/AdminRepositoryCustom.java
@@ -3,10 +3,7 @@ package com.cogent.cogentappointment.admin.repository.custom;
 import com.cogent.cogentappointment.admin.dto.request.admin.AdminInfoRequestDTO;
 import com.cogent.cogentappointment.admin.dto.request.admin.AdminSearchRequestDTO;
 import com.cogent.cogentappointment.admin.dto.request.admin.AdminUpdateRequestDTO;
-import com.cogent.cogentappointment.admin.dto.response.admin.AdminDetailResponseDTO;
-import com.cogent.cogentappointment.admin.dto.response.admin.AdminDropdownDTO;
-import com.cogent.cogentappointment.admin.dto.response.admin.AdminLoggedInInfoResponseDTO;
-import com.cogent.cogentappointment.admin.dto.response.admin.AdminMinimalResponseDTO;
+import com.cogent.cogentappointment.admin.dto.response.admin.*;
 import com.cogent.cogentappointment.persistence.model.Admin;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Pageable;
@@ -37,5 +34,7 @@ public interface AdminRepositoryCustom {
     Admin fetchAdminByUsernameOrEmail(String username);
 
     AdminLoggedInInfoResponseDTO fetchLoggedInAdminInfo(AdminInfoRequestDTO requestDTO);
+
+    LoggedInAdminDTO getLoggedInAdmin(String username);
 }
 

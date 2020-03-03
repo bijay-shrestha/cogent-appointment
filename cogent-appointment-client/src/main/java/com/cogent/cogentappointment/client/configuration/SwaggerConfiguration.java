3dcd4181fdfd71c431625183b518cb8584db817c
sabu shakya <sabu.shakya@f1soft.com>
2020-03-03 16:00:43 +0545

    Npthui

--- cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/configuration/SwaggerConfiguration.java
+++ cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/configuration/SwaggerConfiguration.java
@@ -44,7 +44,7 @@ public class SwaggerConfiguration {
                 "Cogent Appointment System with eSewa Integration (Client)",
                 "HMAC CODE: " +
 //                        "HmacSHA512 NIDA:2a6d801c-589b-4958-b006-807a2fa80677:359180073706848:cW57ipQV7DHgHGCAwwiqdvQfTPPucl4+7iwPT/jeOORp6+cgianmWafrO9iSop8tDhods9IGVf0fdw0T8LHPLw==",
-                        "HmacSHA512 admin:2:COGENT01:649a8184-03f5-4cad-8dfe-e1fef82ce436:423513874946384:opV27wMv9ZtrjADxfNaCZibBNr5w6MsruqstTMRBpoWF3388iDS4P1BZeSATQhUGX9UIQBAmhadyeAicayRrAg==",
+                        "HmacSHA512 sabu:1:HAM01:2a6d801c-589b-4958-b006-807a2fa80677:243275247475329:xKo0ypdExR7gMMHS5+aikKKQjimGwicSpRcGb1tcXq+ve+KgZGTaSo5tofkg8fn22FQ3lgQ+Ygg9usm0iueqjw==",
                 "V1",
                 "http://www.cogenthealth.com.np/",
                 new Contact(

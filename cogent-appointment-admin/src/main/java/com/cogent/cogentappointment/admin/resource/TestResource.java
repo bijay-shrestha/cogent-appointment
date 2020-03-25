f37e6c0758c6b44265813cc1f25b52f217206a9c
smriti.mool <smriti.mool@f1soft.com>
2020-03-25 12:33:38 +0545

    revert

--- cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/resource/TestResource.java
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/resource/TestResource.java
@@ -1,5 +1,6 @@
 package com.cogent.cogentappointment.admin.resource;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @author smriti on 25/03/20
  */
 @RestController
-@RequestMapping(value = "/test")
+@RequestMapping
+@Api("Test")
 public class TestResource {
 
-    @GetMapping
+    @GetMapping("/test")
     public String test() {
         System.out.println(":::: SERVER IS RUNNING ::::");
         return "test done";

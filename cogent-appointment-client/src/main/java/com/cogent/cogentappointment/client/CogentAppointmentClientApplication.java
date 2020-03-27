86c9f63f9ba952ec4ac8bff9679600d4ad10aa69
Bijay Shrestha <bijay.shrestha@f1soft.com>
2020-03-27 12:42:54 +0545

    Package made ready for deployment.

--- cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/CogentAppointmentClientApplication.java
+++ cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/CogentAppointmentClientApplication.java
@@ -4,6 +4,8 @@ import com.cogent.cogentappointment.persistence.util.BeanUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,7 +13,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {"com.cogent.cogentappointment.persistence.model",
         "com.cogent.cogentappointment.persistence.history"})
 @EnableJpaRepositories
-public class CogentAppointmentClientApplication {
+public class CogentAppointmentClientApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CogentAppointmentClientApplication.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(CogentAppointmentClientApplication.class, args);

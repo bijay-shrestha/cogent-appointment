ebb0240bbad71192d57063c49c49412ab3b70e03
f1soft <you@example.com>
2020-04-05 16:31:50 +0545

    allow main class

---
+++ cogent-appointment-esewa/src/main/java/com/cogent/cogentappointment/esewa/CogentAppointmentEsewaApplication.java
@@ -0,0 +1,33 @@
+package com.cogent.cogentappointment.esewa;
+
+import com.cogent.cogentappointment.persistence.util.BeanUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan(basePackages =
+		{"com.cogent.cogentappointment.persistence.model",
+				"com.cogent.cogentappointment.persistence.history"})
+@EnableJpaRepositories
+public class CogentAppointmentEsewaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CogentAppointmentEsewaApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogentAppointmentEsewaApplication.class, args);
+	}
+
+	@Bean
+	public BeanUtil beanUtil() {
+		return new BeanUtil();
+	}
+
+}

ede436f379583429a50b316b5669bef63c254c9f
f1soft <you@example.com>
2020-04-12 11:04:37 +0545

    change total amount query in appoint log details screen

--- cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/query/AppointmentQuery.java
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/query/AppointmentQuery.java
@@ -494,4 +494,22 @@ public class AppointmentQuery {
                     " WHERE ard.appointmentId.id=:appointmentId" +
                     " AND ard.status='PA'";
 
+    public static String QUERY_TO_FETCH_TOTAL_APPOINTMENT_AMOUNT(AppointmentLogSearchDTO searchDTO) {
+        String sql =
+                "SELECT" +
+                        " COALESCE (SUM(atd.appointmentAmount)," +
+                        " 0) as totalAmount" +
+                        " FROM Appointment a" +
+                        " LEFT JOIN Patient p ON a.patientId.id=p.id" +
+                        " LEFT JOIN HospitalPatientInfo hpi ON hpi.patient.id =p.id AND hpi.hospital.id = a.hospitalId.id" +
+                        " LEFT JOIN Doctor d ON d.id = a.doctorId.id" +
+                        " LEFT JOIN Specialization sp ON a.specializationId.id=sp.id" +
+                        " LEFT JOIN Hospital h ON a.hospitalId.id=h.id" +
+                        " LEFT JOIN PatientMetaInfo pi ON pi.patient.id=p.id AND pi.status='Y'" +
+                        " LEFT JOIN AppointmentTransactionDetail atd ON a.id = atd.appointment.id" +
+                        " LEFT JOIN AppointmentRefundDetail ard ON ard.appointmentId=a.id AND ard.status='PA'"
+                        + GET_WHERE_CLAUSE_TO_SEARCH_APPOINTMENT_LOG_DETAILS(searchDTO);
+        return sql;
+    }
+
 }

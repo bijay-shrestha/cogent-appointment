f84940e57ca5a2f900d8381ec85b4a53bd0b6bc9
smriti.mool <smriti.mool@f1soft.com>
2020-04-23 15:31:18 +0545

    add @NotEmpty in specialization

--- cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/dto/request/specialization/SpecializationUpdateRequestDTO.java
+++ cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/dto/request/specialization/SpecializationUpdateRequestDTO.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -23,6 +24,7 @@ public class SpecializationUpdateRequestDTO implements Serializable {
 
     @NotNull
     @NotEmpty
+    @NotBlank
     private String name;
 
     @NotNull
@@ -31,5 +33,6 @@ public class SpecializationUpdateRequestDTO implements Serializable {
 
     @NotNull
     @NotEmpty
+    @NotBlank
     private String remarks;
 }

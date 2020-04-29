0c938bfb601b32fb7b3fbbe2a81861c2193260b3
smriti.mool <smriti.mool@f1soft.com>
2020-04-29 16:29:37 +0545

    add test resource in admin

--- cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/constants/SwaggerConstants.java
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/constants/SwaggerConstants.java
@@ -301,6 +301,10 @@ public class SwaggerConstants {
 
 
     //T
+    public interface TestConstant {
+        String BASE_API_VALUE = "This is Test Resource";
+        String TEST_OPERATION = "This is Test Operation";
+    }
 
 
     //U

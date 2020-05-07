f7e7b895cf1fa5f8a6cc061a6eefcd11ca625c5f
Rupak Chaulagain <rupakchaulagain@gmail.com>
2020-05-07 15:34:03 +0545

    Cache Request filter...

---
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/loghandler/CachingRequestBodyFilter.java
@@ -0,0 +1,43 @@
+package com.cogent.cogentappointment.admin.loghandler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CachingRequestBodyFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper((HttpServletRequest) servletRequest);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper((HttpServletResponse) servletResponse);
+
+        try {
+            chain.doFilter(requestWrapper, responseWrapper);
+            responseWrapper.copyBodyToResponse();
+
+        } finally {
+            String requestBody = new String(requestWrapper.getContentAsByteArray());
+            System.out.println(requestBody);
+
+            String responseBody = new String(responseWrapper.getContentAsByteArray());
+            System.out.println(responseBody);
+
+            // Do not forget this line after reading response content or actual response will be empty!
+//        }
+
+        }
+
+    }
+}
--- cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/loghandler/UserLogInterceptor.java
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/loghandler/UserLogInterceptor.java
@@ -4,15 +4,20 @@ import com.cogent.cogentappointment.admin.dto.commons.AdminLogRequestDTO;
 import com.cogent.cogentappointment.admin.dto.request.login.LoginRequestDTO;
 import com.cogent.cogentappointment.admin.service.AdminLogService;
 import com.cogent.cogentappointment.admin.utils.commons.ObjectMapperUtils;
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
-import javax.servlet.ServletInputStream;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
+import java.io.IOException;
 
 import static com.cogent.cogentappointment.admin.constants.StatusConstants.ACTIVE;
 import static com.cogent.cogentappointment.admin.constants.StatusConstants.INACTIVE;
@@ -24,7 +29,7 @@ import static com.cogent.cogentappointment.admin.loghandler.LogDescription.getSu
 import static com.cogent.cogentappointment.admin.loghandler.RequestHandler.forgotPasswordLogging;
 
 @Component
-public class UserLogInterceptor implements HandlerInterceptor {
+public class UserLogInterceptor extends GenericFilterBean implements HandlerInterceptor {
 
     @Autowired
     private AdminLogService adminLogService;
@@ -37,11 +42,22 @@ public class UserLogInterceptor implements HandlerInterceptor {
         String method = request.getMethod();
         if (uri.contains(API_V1 + LOGIN) && method.equalsIgnoreCase("POST")) {
 
-            ServletInputStream a = request.getInputStream();
-            String requestStr = IOUtils.toString(a);
-            LoginRequestDTO adminRequestDTO = ObjectMapperUtils.map(requestStr, LoginRequestDTO.class);
+            ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+            ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
 
-            System.out.println(adminRequestDTO.toString());
+            String swaggerJson = new String(requestWrapper.getContentAsByteArray());
+
+            LoginRequestDTO requestDTO = ObjectMapperUtils.map(swaggerJson, LoginRequestDTO.class);
+
+            System.out.println(swaggerJson);
+
+//            ObjectMapper mapper = new ObjectMapper();
+//            try {
+//                String json = mapper.writeValueAsString(cat);
+//                System.out.println("ResultingJSONstring = " + json);
+//                //System.out.println(json);
+//            } catch (JsonProcessingException e) {
+//                e.printStackTrace();
 
 
 //            getInputStream(requestWrapper.getContentAsByteArray());
@@ -100,5 +116,27 @@ public class UserLogInterceptor implements HandlerInterceptor {
 
     private void saveFailedLogs(AdminLogRequestDTO adminLogRequestDTO) {
         adminLogService.save(adminLogRequestDTO, INACTIVE);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper((HttpServletRequest) servletRequest);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper((HttpServletResponse) servletResponse);
+
+        try {
+            filterChain.doFilter(requestWrapper, responseWrapper);
+            responseWrapper.copyBodyToResponse();
+
+        } finally {
+            String requestBody = new String(requestWrapper.getContentAsByteArray());
+            System.out.println(requestBody);
+
+            String responseBody = new String(responseWrapper.getContentAsByteArray());
+            System.out.println(responseBody);
+
+            // Do not forget this line after reading response content or actual response will be empty!
+        }
+
     }
 }

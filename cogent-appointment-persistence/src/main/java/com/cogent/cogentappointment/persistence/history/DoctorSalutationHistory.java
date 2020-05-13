d4fd06847c02a93cbf7616aba17807e2b4fe9272
Rupak Chaulagain <rupakchaulagain@gmail.com>
2020-05-13 14:32:43 +0545

    listener added...

---
+++ cogent-appointment-persistence/src/main/java/com/cogent/cogentappointment/persistence/history/DoctorSalutationHistory.java
@@ -0,0 +1,9 @@
+package com.cogent.cogentappointment.persistence.history;
+
+import com.cogent.cogentappointment.persistence.config.Action;
+import com.cogent.cogentappointment.persistence.model.DoctorSalutation;
+
+public class DoctorSalutationHistory {
+    public DoctorSalutationHistory(DoctorSalutation target, Action action) {
+    }
+}

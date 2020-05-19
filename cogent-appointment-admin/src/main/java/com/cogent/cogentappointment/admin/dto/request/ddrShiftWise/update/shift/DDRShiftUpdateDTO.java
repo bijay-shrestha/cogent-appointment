ef69e7df11de9cb89071e4ed39c2607ceedb22f2
smriti.mool <smriti.mool@f1soft.com>
2020-05-19 10:52:58 +0545

    add dtos

---
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/dto/request/ddrShiftWise/update/shift/DDRShiftUpdateDTO.java
@@ -0,0 +1,20 @@
+package com.cogent.cogentappointment.admin.dto.request.ddrShiftWise.update.shift;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * @author smriti on 17/05/20
+ */
+@Getter
+@Setter
+public class DDRShiftUpdateDTO implements Serializable {
+
+    private Long ddrShiftDetailId;
+
+    private Integer rosterGapDuration;
+
+    private Character status;
+}
---
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/dto/request/ddrShiftWise/update/shift/DDRShiftUpdateRequestDTO.java
@@ -0,0 +1,7 @@
+package com.cogent.cogentappointment.admin.dto.request.ddrShiftWise.update.shift;
+
+/**
+ * @author smriti on 19/05/20
+ */
+public class DDRShiftUpdateRequestDTO {
+}

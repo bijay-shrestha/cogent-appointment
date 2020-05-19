19b7247ae5dc1da405f2d33115f3e5f96f042389
Rupak Chaulagain <rupakchaulagain@gmail.com>
2020-05-19 10:09:12 +0545

    Refund % added in response of appointment....

--- cogent-appointment-esewa/src/main/java/com/cogent/cogentappointment/esewa/dto/response/appointment/save/AppointmentSuccessResponseDTO.java
+++ cogent-appointment-esewa/src/main/java/com/cogent/cogentappointment/esewa/dto/response/appointment/save/AppointmentSuccessResponseDTO.java
@@ -17,6 +17,8 @@ public class AppointmentSuccessResponseDTO implements Serializable {
 
     private String appointmentNumber;
 
+    private Double refundPercentage;
+
     private Character appointmentTransactionStatus;
 
     private int responseCode;

1f04e19b248188dc3c5f965def7236337aa63804
Rupak Chaulagain <rupakchaulagain@gmail.com>
2020-05-25 15:31:00 +0545

    Search and Delete API Integrated ffor client api feature integration

---
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/dto/request/clientIntegration/ClientApiIntegrationSearchRequestDTO.java
@@ -0,0 +1,21 @@
+package com.cogent.cogentappointment.admin.dto.request.clientIntegration;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * @author rupak on 2020-05-25
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientApiIntegrationSearchRequestDTO implements Serializable {
+
+    private Long hospitalId;
+
+    private Long featureTypeId;
+
+}
---
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/dto/response/clientIntegration/ClientApiIntegrationDetailResponseDTO.java
@@ -0,0 +1,30 @@
+package com.cogent.cogentappointment.admin.dto.response.clientIntegration;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * @author rupak on 2020-05-25
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientApiIntegrationDetailResponseDTO  implements Serializable {
+
+    private String featureCode;
+
+    private String requestMethod;
+
+    private String url;
+
+    private String requestBody;
+
+    private Map<String,String> headers;
+
+    private Map<String,String> queryParameters;
+
+}

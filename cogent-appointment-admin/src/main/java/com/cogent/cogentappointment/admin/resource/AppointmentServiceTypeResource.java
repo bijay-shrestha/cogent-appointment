f59134d1c33c5450f67cb3784c125efef9585beb
smriti.mool <smriti.mool@f1soft.com>
2020-05-26 14:39:29 +0545

    add app service type fetch api

--- cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/resource/AppointmentServiceTypeResource.java
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/resource/AppointmentServiceTypeResource.java
@@ -1,12 +1,18 @@
 package com.cogent.cogentappointment.admin.resource;
 
+import com.cogent.cogentappointment.admin.service.AppointmentServiceTypeService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.cogent.cogentappointment.admin.constants.SwaggerConstants.AppointmentServiceTypeConstant.BASE_API_VALUE;
-import static com.cogent.cogentappointment.admin.constants.WebResourceKeyConstants.API_V1;
+import static com.cogent.cogentappointment.admin.constants.SwaggerConstants.AppointmentServiceTypeConstant.FETCH_DETAILS_FOR_DROPDOWN;
+import static com.cogent.cogentappointment.admin.constants.WebResourceKeyConstants.*;
 import static com.cogent.cogentappointment.admin.constants.WebResourceKeyConstants.AppointmentServiceTypeConstants.BASE_APPOINTMENT_SERVICE_TYPE;
+import static org.springframework.http.ResponseEntity.ok;
 
 /**
  * @author smriti on 26/05/20
@@ -15,4 +21,17 @@ import static com.cogent.cogentappointment.admin.constants.WebResourceKeyConstan
 @RestController
 @Api(BASE_API_VALUE)
 public class AppointmentServiceTypeResource {
+
+    private final AppointmentServiceTypeService appointmentServiceTypeService;
+
+    public AppointmentServiceTypeResource(AppointmentServiceTypeService appointmentServiceTypeService) {
+        this.appointmentServiceTypeService = appointmentServiceTypeService;
+    }
+
+    @GetMapping(ACTIVE + MIN)
+    @ApiOperation(FETCH_DETAILS_FOR_DROPDOWN)
+    public ResponseEntity<?> fetchActiveMinInfo() {
+        return ok(appointmentServiceTypeService.fetchActiveMinInfo());
+    }
+
 }
--- cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/service/AppointmentServiceTypeService.java
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/service/AppointmentServiceTypeService.java
@@ -1,7 +1,16 @@
 package com.cogent.cogentappointment.admin.service;
 
+import com.cogent.cogentappointment.admin.dto.commons.DropDownResponseDTO;
+import com.cogent.cogentappointment.persistence.model.AppointmentServiceType;
+
+import java.util.List;
+
 /**
  * @author smriti on 26/05/20
  */
 public interface AppointmentServiceTypeService {
+
+    List<DropDownResponseDTO> fetchActiveMinInfo();
+
+    AppointmentServiceType fetchById(Long id);
 }

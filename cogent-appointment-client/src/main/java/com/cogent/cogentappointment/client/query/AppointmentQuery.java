fc57943d2c83d1009a2ead054ac538ef237a9ac3
Rupak Chaulagain <rupakchaulagain@gmail.com>
2020-05-31 14:38:00 +0545

    Hospital Nubmer added...

--- cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/query/AppointmentQuery.java
+++ cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/query/AppointmentQuery.java
@@ -604,7 +604,8 @@ public class AppointmentQuery {
                     " da.fileUri as fileUri," +
                     " a.doctorId.id as doctorId," +
                     " a.specializationId.id as specializationId," +
-                    " a.isFollowUp as followUp" +                                               //[18]
+                    " a.isFollowUp as followUp," +                                               //[18]
+                    " hpi.hospitalNumber as hospitalNumber"+
                     " FROM Appointment a" +
                     " LEFT JOIN Patient p ON a.patientId=p.id" +
                     " LEFT JOIN HospitalPatientInfo hpi ON hpi.patient.id =p.id AND hpi.hospital.id = a.hospitalId.id" +

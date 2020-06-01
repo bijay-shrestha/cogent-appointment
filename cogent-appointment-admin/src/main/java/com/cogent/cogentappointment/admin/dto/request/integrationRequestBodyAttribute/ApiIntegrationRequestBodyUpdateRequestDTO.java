c5cd3386d0e9af6e4eb1f2fda23a8d520362163c
Rupak Chaulagain <rupakchaulagain@gmail.com>
2020-06-01 11:52:49 +0545

    Code refactored...

---
+++ cogent-appointment-admin/src/main/java/com/cogent/cogentappointment/admin/dto/request/integrationRequestBodyAttribute/ApiIntegrationRequestBodyUpdateRequestDTO.java
@@ -0,0 +1,21 @@
+package com.cogent.cogentappointment.admin.dto.request.integrationRequestBodyAttribute;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author rupak ON 2020/06/01-11:47 AM
+ */
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiIntegrationRequestBodyUpdateRequestDTO implements Serializable {
+
+
+
+}

6425be1939fd91bb78c19927e1b887ebfc036338
f1soft <you@example.com>
2020-06-04 18:09:53 +0545

    add billingMode Id

--- cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/query/HospitalDepartmentQuery.java
+++ cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/query/HospitalDepartmentQuery.java
@@ -173,7 +173,8 @@ public class HospitalDepartmentQuery {
 
     public static String QUERY_TO_FETCH_HOSPITAL_DEPARTMENT_BILLING_MODE_WITH_CHARGE=
             "SELECT " +
-                    "  hb.id as id, " +
+                    "  hb.id as id," +
+                    "  hb.billingMode.id as billingModeId, " +
                     "  hb.billingMode.name as billingMode, " +
                     "  hb.appointmentCharge as appointmentCharge, " +
                     "  hb.appointmentFollowUpCharge as followUpCharge" +

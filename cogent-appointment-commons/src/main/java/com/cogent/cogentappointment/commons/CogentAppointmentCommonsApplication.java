a21ddfecda90cfcb56f1e3d12d1b145d9e544296
f1soft <you@example.com>
2020-06-05 11:11:18 +0545

    fix

---
+++ cogent-appointment-commons/src/main/java/com/cogent/cogentappointment/commons/CogentAppointmentCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.cogent.cogentappointment.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogentAppointmentCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogentAppointmentCommonsApplication.class, args);
+	}
+
+}

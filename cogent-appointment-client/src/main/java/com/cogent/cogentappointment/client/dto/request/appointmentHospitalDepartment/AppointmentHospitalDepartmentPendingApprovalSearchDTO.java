1c2680f4f415d8348cab5a4bca76726d37f0c6d2
smriti.mool <smriti.mool@f1soft.com>
2020-06-15 11:24:06 +0545

    test

--- cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/dto/request/appointmentHospitalDepartment/AppointmentHospitalDepartmentPendingApprovalSearchDTO.java
+++ cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/dto/request/appointmentHospitalDepartment/AppointmentHospitalDepartmentPendingApprovalSearchDTO.java
@@ -1,4 +1,4 @@
-package com.cogent.cogentappointment.admin.dto.request.appointment.appointmentHospitalDepartment;
+package com.cogent.cogentappointment.client.dto.request.appointmentHospitalDepartment;
 
 import lombok.Getter;
 import lombok.Setter;

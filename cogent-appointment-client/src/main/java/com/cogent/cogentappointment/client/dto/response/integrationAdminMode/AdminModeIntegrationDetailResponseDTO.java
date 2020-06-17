0c3a62c53da96981fcc86bc646d67431ebd36c69
Rupak Chaulagain <rupakchaulagain@gmail.com>
2020-06-17 17:10:51 +0545

    bug fixes...

--- cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/dto/response/integrationAdminMode/AdminModeIntegrationDetailResponseDTO.java
+++ cogent-appointment-client/src/main/java/com/cogent/cogentappointment/client/dto/response/integrationAdminMode/AdminModeIntegrationDetailResponseDTO.java
@@ -35,6 +35,7 @@ public class AdminModeIntegrationDetailResponseDTO implements Serializable {
 
     private String url;
 
+
     private List<ApiRequestHeaderDetailResponse> headers;
 
     private List<ApiQueryParametersDetailResponse> queryParameters;

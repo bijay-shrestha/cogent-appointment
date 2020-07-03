64ab46b0b4fd1f25fbfd8776426461dbb3d13be7
Rupak Chaulagain <rupakchaulagain@gmail.com>
2020-07-03 17:27:12 +0545

    code refactroed...

--- cogent-appointment-esewa/src/main/java/com/cogent/cogentappointment/esewa/query/EsewaQuery.java
+++ cogent-appointment-esewa/src/main/java/com/cogent/cogentappointment/esewa/query/EsewaQuery.java
@@ -188,7 +188,6 @@ public class EsewaQuery {
                 " END as fileUri," +                             //[5]
                 " tbl1.qualificationAlias as qualificationAlias," +  //[6]
                 " d.salutation as doctorSalutation,"+
-                " tbl1.qualificationAlias as qualificationAlias," + //[6]
                 " dc.appointment_charge as appointmentCharge"+      //[7]
                 " FROM doctor_duty_roster ddr" +
                 " LEFT JOIN doctor_week_days_duty_roster dw ON dw.doctor_duty_roster_id = ddr.id" +

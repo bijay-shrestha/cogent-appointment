8ca5dea0cbbdd34cdba7b7dfec2d1fd561f8d2b5
smriti.mool <smriti.mool@f1soft.com>
2020-07-06 18:49:30 +0545

    add pki

--- cogent-appointment-esewa/src/main/java/com/cogent/cogentappointment/esewa/scheduler/AppointmentHospitalDepartmentReservationScheduler.java
+++ cogent-appointment-esewa/src/main/java/com/cogent/cogentappointment/esewa/scheduler/AppointmentHospitalDepartmentReservationScheduler.java
@@ -4,11 +4,6 @@ import com.cogent.cogentappointment.esewa.service.AppointmentHospitalDepartmentR
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-
-import static com.cogent.cogentappointment.esewa.constants.SchedulerConstants.APPOINTMENT_RESERVATION_SCHEDULER;
-import static com.cogent.cogentappointment.esewa.constants.WebResourceKeyConstants.SCHEDULER_RUNNING;
-import static com.cogent.cogentappointment.esewa.log.constants.AppointmentReservationLog.APPOINTMENT_RESERVATION_LOG;
 
 /**
  * @author smriti on 18/02/20
@@ -25,10 +20,10 @@ public class AppointmentHospitalDepartmentReservationScheduler {
         this.appointmentHospitalDepartmentReservationLogService = appointmentHospitalDepartmentReservationLogService;
     }
 
-    /*RUNS IN 2 MINS*/
-    @Scheduled(fixedDelayString = APPOINTMENT_RESERVATION_SCHEDULER)
-    public void deleteExpiredAppointmentReservation() {
-        log.info(SCHEDULER_RUNNING, APPOINTMENT_RESERVATION_LOG);
-        appointmentHospitalDepartmentReservationLogService.deleteExpiredAppointmentHospitalDeptReservation();
-    }
+//    /*RUNS IN 2 MINS*/
+//    @Scheduled(fixedDelayString = APPOINTMENT_RESERVATION_SCHEDULER)
+//    public void deleteExpiredAppointmentReservation() {
+//        log.info(SCHEDULER_RUNNING, APPOINTMENT_RESERVATION_LOG);
+//        appointmentHospitalDepartmentReservationLogService.deleteExpiredAppointmentHospitalDeptReservation();
+//    }
 }

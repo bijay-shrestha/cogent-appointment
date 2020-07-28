499db3037dd25bbbce66c17f215816fbebcd94bf
smreeti <mool.smreeti@gmail.com>
2020-07-28 13:57:20 +0545

    add doctor image

--- cogent-appointment-esewa/src/main/java/com/cogent/cogentappointment/esewa/repository/custom/impl/HospitalDepartmentWeekDaysDutyRosterDoctorInfoRepositoryCustomImpl.java
+++ cogent-appointment-esewa/src/main/java/com/cogent/cogentappointment/esewa/repository/custom/impl/HospitalDepartmentWeekDaysDutyRosterDoctorInfoRepositoryCustomImpl.java
@@ -1,5 +1,6 @@
 package com.cogent.cogentappointment.esewa.repository.custom.impl;
 
+import com.cogent.cogentappointment.commons.configuration.MinIOProperties;
 import com.cogent.cogentappointment.esewa.dto.response.appointmentHospitalDepartment.checkAvailability.HospitalDepartmentDoctorInfoResponseDTO;
 import com.cogent.cogentappointment.esewa.repository.custom.HospitalDepartmentWeekDaysDutyRosterDoctorInfoRepositoryCustom;
 import lombok.extern.slf4j.Slf4j;
@@ -11,8 +12,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
 
-import static com.cogent.cogentappointment.esewa.constants.QueryConstants.CODE;
-import static com.cogent.cogentappointment.esewa.constants.QueryConstants.HOSPITAL_DEPARTMENT_DUTY_ROSTER_ID;
+import static com.cogent.cogentappointment.esewa.constants.QueryConstants.*;
 import static com.cogent.cogentappointment.esewa.query.HospitalDepartmentWeekDaysDutyRosterDoctorInfoQuery.QUERY_TO_FETCH_AVAILABLE_DOCTORS;
 import static com.cogent.cogentappointment.esewa.utils.commons.QueryUtils.createQuery;
 import static com.cogent.cogentappointment.esewa.utils.commons.QueryUtils.transformQueryToResultList;
@@ -29,11 +29,18 @@ public class HospitalDepartmentWeekDaysDutyRosterDoctorInfoRepositoryCustomImpl 
     @PersistenceContext
     private EntityManager entityManager;
 
+    private final MinIOProperties minIOProperties;
+
+    public HospitalDepartmentWeekDaysDutyRosterDoctorInfoRepositoryCustomImpl(MinIOProperties minIOProperties) {
+        this.minIOProperties = minIOProperties;
+    }
+
     @Override
     public List<HospitalDepartmentDoctorInfoResponseDTO> fetchAvailableDoctors(Long hddRosterId, String weekDayCode) {
 
         Query query = createQuery.apply(entityManager, QUERY_TO_FETCH_AVAILABLE_DOCTORS)
                 .setParameter(HOSPITAL_DEPARTMENT_DUTY_ROSTER_ID, hddRosterId)
+                .setParameter(CDN_URL, minIOProperties.getCDN_URL())
                 .setParameter(CODE, weekDayCode);
 
         return transformQueryToResultList(query, HospitalDepartmentDoctorInfoResponseDTO.class);
